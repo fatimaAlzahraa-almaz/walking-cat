b39bc06300592724692765859544fb4e148f6680
fatima alzahraa almaz <almazfatima9@gmail.com>
2025-05-30 20:13:26 +0300

    Update walk.js

--- walk.js
+++ walk.js
@@ -1,8 +1,20 @@
-
- function funca(b='Character_spritesheet')
+ 
+ function handle(b='Character_spritesheet')
 {
+   var s=document.getElementById('button-sound');
+  s.currentTime = 0;
+   s.playbackRate = 1.5;
+  s.play();
 let a=document.getElementById('ct');
 a.className = '';
 a.className += `   Character_spritesheet pixelart ${b}`;
 console.log(a.className);
 }
+function sound()
+{
+   var s=document.getElementById('song');
+  s.currentTime = 0;
+   s.playbackRate = 1;
+  s.play();
+}
+ 
